2a593cad9a45a70c7396e580fa580456d25d3619
vKS-Rajput <k8659653@gmail.com>
2025-07-14 12:03:26 +0530

    Link Updated

--- leaderboard-project/leaderboard-frontend/src/Socket.js
+++ leaderboard-project/leaderboard-frontend/src/Socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); // backend address
+const socket = io('https://leaderboard-project-alpha.vercel.app/'); // backend address
 export default socket;
